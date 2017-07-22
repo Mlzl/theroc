4bd4a8eb88f4325af4c373ce2253a329e542a366
jiongrong <1446418561@qq.com>
2017-07-22 15:51:04 +0800

    支持页面

--- app/cache/d%%%%project%%theroc%%app%%view%%frontend%%pages%%support.volt.php
+++ app/cache/d%%%%project%%theroc%%app%%view%%frontend%%pages%%support.volt.php
@@ -11,7 +11,21 @@
 <body>
 <div id="support" v-cloak>
     <!--头部导航栏-->
-    {% include "smart/topBar.volt" %}
+    <div class="topBar">
+    <img src="/images/logo.png" class="logo"/>
+    <ul class="topBar_menu">
+        <li>HOME</li>
+        <li>PRODUCTS</li>
+        <li>COMMUNITY</li>
+        <li>CONTANT US</li>
+    </ul>
+    <div class="topBar_right">
+        <p class="search">
+            <input type="text" />
+        </p>
+        <i class="user_icon"></i>
+    </div>
+</div>
     <div class="support-content">
         <div class="banner-container">
             <img  class="img" src="/frontend/image/supportBg.jpg">
@@ -175,7 +189,42 @@
         </div>
     </div>
     <!--底部-->
-    {% include "smart/footer.volt" %}
+    <div class="footer">
+    <div class="subscribe">
+        <h4>Subscribe</h4>
+        <div class="email_submit">
+            <p>Be the first to know bout our lastest products</p>
+            <input type="text" placeholder="Enter your email address"/><br/>
+            <el-button>SUBMIT</el-button>
+        </div>
+        <div class="link">
+
+        </div>
+    </div>
+    <div class="about">
+        <h4>About</h4>
+        <ul>
+            <li><span>Contant us</span></li>
+        </ul>
+    </div>
+    <div class="support">
+        <h4>Support</h4>
+        <ul>
+            <li><span>Refund & Exchange</span></li>
+            <li><span>Warranty</span></li>
+        </ul>
+    </div>
+    <div class="contact">
+        <div class="call">
+            <h4>Call us</h4>
+            <p>13560455672</p>
+        </div>
+        <div class="email">
+            <h4>Email</h4>
+            <p>245452530@qq.com</p>
+        </div>
+    </div>
+</div>
 </div>
 <script type="text/javascript" src="/plugin/vue.js"></script>
 <!--<script src="https://unpkg.com/element-ui/lib/index.js"></script>-->
---
+++ app/cache/d%%%%project%%theroc%%app%%view%%frontend%%smart%%topbar.volt.php
@@ -0,0 +1,15 @@
+<div class="topBar">
+    <img src="/images/logo.png" class="logo"/>
+    <ul class="topBar_menu">
+        <li>HOME</li>
+        <li>PRODUCTS</li>
+        <li>COMMUNITY</li>
+        <li>CONTANT US</li>
+    </ul>
+    <div class="topBar_right">
+        <p class="search">
+            <input type="text" />
+        </p>
+        <i class="user_icon"></i>
+    </div>
+</div>
