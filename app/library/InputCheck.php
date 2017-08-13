2c82de41799a4a2902e25efb15ae2d74f470cf30
liuzhilang <liuzl@mama.cn>
2017-08-13 14:22:54 +0800

    fix

--- app/library/InputCheck.php
+++ app/library/InputCheck.php
@@ -17,8 +17,12 @@ class InputCheck extends Library {
     }
 
     public static function isValidMoney($money, $precision=2){
+        if(intval($money) == $money){
+            return true;
+        }
         $pattern = "/^\d{1,}\.\d{1,$precision}$/";
         preg_match($pattern, $money, $match);
+
         if(preg_match($pattern, $money)){
             return true;
         }
